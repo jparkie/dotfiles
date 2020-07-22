59df9be5d5f57a9ee020df104d1356c666ebda61
Jacob Park <park.jacob.96@gmail.com>
2020-07-22 01:02:31 -0400

    Updated zsh/

--- zsh/.zsh/compinit.zsh
+++ zsh/.zsh/compinit.zsh
@@ -1,5 +1,16 @@
 #!/usr/local/bin/zsh
 
+# Completion System:
 zstyle :compinstall filename '/Users/jparkie/.zshrc'
 autoload -Uz compinit
 compinit
+
+# jEnv:
+if command -v jenv 1>/dev/null 2>&1; then
+  eval "$(jenv init -)"
+fi
+
+# pyenv:
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,3 +1,4 @@
 #!/usr/local/bin/zsh
 
 export PATH="/usr/local/bin:$PATH"
+export PATH="$HOME/.jenv/bin:$PATH"
